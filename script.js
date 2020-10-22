bb2baeccf8d78ffbc3e2b41a4ded593a97136ecc
Frank Buono <fbuonojr@fordham.edu>
2020-10-22 16:50:58 -0400

    set up movie display function

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //list of movies to be displayed automatically
 var movies = ["The Godfather", "The Dark Knight Rises", "Fight Club", "Catch Me If You Can"];
+
+//this function renders the html
+function displayMovieInfo() {
+    
+}
